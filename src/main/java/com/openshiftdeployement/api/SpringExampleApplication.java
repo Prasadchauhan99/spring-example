5c6373bed5540b00be4810a6623d4f77d7fd0fa8
prasad chauhan <67370566+Prasasd8@users.noreply.github.com>
2022-07-28 21:03:21 +0530

    Add files via upload

---
+++ src/main/java/com/openshiftdeployement/api/SpringExampleApplication.java
@@ -0,0 +1,29 @@
+package com.openshiftdeployement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringExampleApplication {
+	
+	@GetMapping("/")
+	public String welcome()
+	{
+		return "welcome to java prasad";
+	}
+	
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input)
+	{
+		return "hi"+input+"Your Application deployed successfully.........";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
